b3b773cab6a728bc11f7e3293df59b5f6a18923a
will-cesar <willcesar010421@gmail.com>
2021-08-10 22:29:42 -0300

    Criação do componente register, ajuste no prettier e no tsconfig

---
+++ medium-clone-app/src/app/auth/components/register/register.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  initializeForm(): void {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  onSubmit(): void {}
+}
