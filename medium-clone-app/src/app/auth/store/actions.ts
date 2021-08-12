6d0a5ddb1f99a3ba248fcb81b19288537ae98e31
will-cesar <willcesar010421@gmail.com>
2021-08-11 22:15:04 -0300

    Criação das interfaces de autenticação

--- medium-clone-app/src/app/auth/store/actions.ts
+++ medium-clone-app/src/app/auth/store/actions.ts
@@ -1,7 +1,8 @@
 import {createAction, props} from '@ngrx/store';
+import {RegisterRequestInterface} from 'src/app/shared/types/register-request.interface';
 import {ActionTypes} from './action-types.enum';
 
 export const registerAction = createAction(
   ActionTypes.REGISTER,
-  props<{username: string; password: string; email: string}>()
+  props<{request: RegisterRequestInterface}>()
 );
